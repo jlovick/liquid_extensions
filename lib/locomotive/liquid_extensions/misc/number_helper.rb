d16f586f83e7726fa441cb0702c8ec520e8d8abd
Joe Lovick <joh3@anatexis.com>
2016-05-16 15:59:51 -0600

    fix require

--- lib/locomotive/liquid_extensions/misc/number_helper.rb
+++ lib/locomotive/liquid_extensions/misc/number_helper.rb
@@ -2,12 +2,12 @@
 
 unless defined?(Rails)
 
-  require 'active_support/core_ext/hash/keys'
-  require 'active_support/core_ext/hash/reverse_merge'
-  require 'active_support/core_ext/big_decimal/conversions'
-  require 'active_support/core_ext/float/rounding'
-  require 'active_support/core_ext/object/blank'
-  require 'active_support/core_ext/string/output_safety'
+  require 'active_support/core_ext'
+#  require 'active_support/core_ext/hash/reverse_merge'
+#  require 'active_support/core_ext/big_decimal/conversions'
+#  require 'active_support/core_ext/float/rounding'
+#  require 'active_support/core_ext/object/blank'
+#  require 'active_support/core_ext/string/output_safety'
 
   module ActionView
     # = Action View Number Helpers
